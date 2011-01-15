2a5328e7be27b7415ed96757d3f89115f8108fb4
sigilioso <sigilioso@gmail.com>
2011-01-15 14:32:43 +0100

    Various changes in functions to color.

--- colors.py
+++ colors.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 
-def color(pixel, indexes=(0,1,2), increment=90):
-	for i in indexes:
+def color(pixel, colour='blue', increment=90):
+	indexes = dict(red=(0,), green=(1,), blue=(2,), gray=(0,1,2))
+	for i in indexes[colour]:
 		if pixel[i] != 0: pixel[i] = pixel[i] + increment \
 			if pixel[i] + increment < 255 else 255
 	return pixel
 
-def simple_color(pixel, color='blue'):
-
-	
-
-def color_pixels(p, maxIt):
+def color_pixels(p, maxIt, colour='blue'):
 	"""
 	Defines the color of a pixel depending on the distance from the point p to the set
 	"""
 	def to_byte(e):
 		return 0 if e == maxIt else 255 * e / maxIt
 	pixel = map(to_byte, [p, p, p])
-	if pixel[2] != 0: pixel[2] = pixel[2] + 90 if pixel[2] + 90 < 255 else 255
-	return tuple(pixel)
+	return tuple(color(pixel, colour))
 
 
